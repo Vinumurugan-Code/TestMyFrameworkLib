483191b428afb849f16b5b98e5c837d50a413d8f
Vinumurugan-Code <vinuios15@gmail.com>
2024-02-20 23:40:41 +0530

    Initial Commit

---
+++ TestMyFrameworkLib/TestMyFrameworkLib.h
@@ -0,0 +1,18 @@
+//
+//  TestMyFrameworkLib.h
+//  TestMyFrameworkLib
+//
+//  Created by Vinumurugan on 20/02/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestMyFrameworkLib.
+FOUNDATION_EXPORT double TestMyFrameworkLibVersionNumber;
+
+//! Project version string for TestMyFrameworkLib.
+FOUNDATION_EXPORT const unsigned char TestMyFrameworkLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestMyFrameworkLib/PublicHeader.h>
+
+
